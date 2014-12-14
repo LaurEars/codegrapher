3356cd0c5c85a09107a6ba48e028a54eb5ca076c
LaurEars <laura.rupprecht@zefr.com>
2014-12-13 22:05:03 -0500

    Add graphviz file output argument

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import ast
 
 import click
 
+from graphing.graph import FunctionGrapher
 from parsing.parser import FileVisitor
 
 
@@ -9,7 +10,8 @@ from parsing.parser import FileVisitor
 @click.argument('code', type=click.File('rb'))
 @click.option('--printed', default=False, is_flag=True, help='Pretty prints the call tree for each class in the file')
 @click.option('--remove-builtins', default=False, is_flag=True, help='Removes builtin functions from call trees')
-def cli(code, printed, remove_builtins):
+@click.option('--output', help='Graphviz output file name')
+def cli(code, printed, remove_builtins, output):
     """
     Parses a file.
     codegrapher [file_name]
@@ -26,3 +28,11 @@ def cli(code, printed, remove_builtins):
             click.echo(class_object.name)
             click.echo(class_object.pprint())
             click.echo('')
+    if output:
+        graph = FunctionGrapher()
+        class_names = set(cls.name for cls in visitor.classes)
+        for cls in visitor.classes:
+            graph.add_dict_to_graph(class_names, cls.call_tree)
+        graph.add_classes_to_graph(visitor.classes)
+        graph.name = output
+        graph.render()
